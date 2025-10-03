25f572f112632dc4ad27fc2ec48e80ed48c87e3c
willzyx-hub <williamliu636@gmail.com>
2025-10-03 18:30:48 +0700

    Try to Improve Google Chrome Installation logic

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,9 +20,36 @@ if [ -e /opt ]; then
   fi
 fi
 
-# Install Google Chrome
-wget -O /tmp/google-chrome-stable_current_x86_64.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm # Try using absolute path
-dnf5 install -y /tmp/google-chrome-stable_current_x86_64.rpm
+# Prepare Google Chrome Installation
+mkdir -p /var/opt
+
+# Add Google Chrome repository
+cat << EOF > /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=google-chrome
+baseurl=https://dl.google.com/linux/chrome/rpm/stable/\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-google
+EOF
+
+# Add Google Chrome signing keys
+curl --retry 3 --retry-delay 2 --retry-all-errors -sL \
+  -o /etc/pki/rpm-gpg/RPM-GPG-KEY-google \
+  https://dl.google.com/linux/linux_signing_key.pub
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-google
+
+# Install Google Chrome via dng
+dnf5 install -y google-chrome-stable
+
+# Clean up the yum repo
+rm -f /etc/yum.repos.d/google-chrome.repo
+
+# Move the application to somewhere on the final image
+mv /var/opt/google /usr/lib/google
+echo "Google Chrome Installation Completed successfully"
+
 
 # Use a COPR Example:
 #
