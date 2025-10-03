95f59276df87f98b185c2c72f5dd6a2a65cd4d39
willzyx-hub <williamliu636@gmail.com>
2025-10-03 17:43:14 +0700

    Try different approach

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,8 +13,8 @@ set -ouex pipefail
 dnf5 install -y tmux virt-manager sbsigntools gparted terminus-fonts terminus-fonts-console
 
 # Install Google Chrome
-curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm . # Try to add . to ensure the .rpm downloaded at current directory
-dnf5 install -y ./google-chrome-stable_current_x86.64.rpm
+wget -O /tmp/google-chrome-stable_current_x86_64.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm # Try using absolute path
+dnf5 install -y /tmp/google-chrome-stable_current_x86_64.rpm
 
 # Use a COPR Example:
 #
