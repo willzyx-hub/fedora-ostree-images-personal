53d9cb3a7a64a9b5bc80281bc7665013f634fee2
willzyx-hub <williamliu636@gmail.com>
2025-10-16 11:08:27 +0700

    Try to fix build failure

--- build_files/kernel-override.sh
+++ build_files/kernel-override.sh
@@ -3,6 +3,9 @@ set -ouex pipefail
 
 KERNEL_VERSION="6.16.9-200.fc42.x86_64"
 
+# Clear Bluefin version Lock
+dnf5 versionlock clear || true
+
 # Check if kernel exist on repository
 
 if ! dnf5 info -q kernel-core-${KERNEL_VERSION} > /dev/null 2>&1; then
@@ -20,3 +23,6 @@ dnf5 install -y kernel-core-${KERNEL_VERSION} kernel-modules-${KERNEL_VERSION} k
 
 # Lock kernel update for future builds
 echo "exclude=kernel* kernel-core* kernel-modules* kernel-modules-extra*" >> /etc/dnf/dnf.conf
+
+# Restore kernel version lock
+dnf5 versionlock add kernel-core-${KERNEL_VERSION} kernel-modules-${KERNEL_VERSION} kernel-modules-extra-${KERNEL_VERSION}
